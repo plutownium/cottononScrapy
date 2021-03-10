92d8370a3aae04b643fcd7b1a09170026bf58ece
plutownium <rolandmackintosh@outlook.com>
2021-03-10 11:52:14 -0800

    fixed bug concatenating links; sent scrapy to view each product using requests

--- cottonon/cottonon/spiders/learnScrapy.py
+++ cottonon/cottonon/spiders/learnScrapy.py
@@ -2,6 +2,7 @@ import scrapy
 import logging
 from bs4 import BeautifulSoup
 from math import ceil
+import requests
 
 logging.getLogger('scrapy').propagate = False
 
@@ -16,6 +17,8 @@ class Product(scrapy.Item):
     name = scrapy.Field()
     price = scrapy.Field()
     colors = scrapy.Field()
+    images = scrapy.Field()
+    ratings = scrapy.Field()
 
 
 class CottononSpider(scrapy.Spider):
@@ -40,7 +43,7 @@ class CottononSpider(scrapy.Spider):
                         for title in menu_item.css('a::attr(data-gtag)').getall():
                             menu_items_to_print.append(title)
 
-                    print("ASDASDFDFDSFDSS", len(categories_of_products))
+                    # print("ASDASDFDFDSFDSS", len(categories_of_products))
                     # categories_of_products is a set of links like:
                     # https://cottonon.com/AU/women/womens-activewear/
                     # https://cottonon.com/AU/women/womens-activewear/womens-gym-tops/
@@ -49,10 +52,10 @@ class CottononSpider(scrapy.Spider):
                     # https://cottonon.com/AU/women/womens-activewear/womens-gym-active-fleece/
                     # https://cottonon.com/AU/women/womens-activewear/womens-running-jackets-vests/
                     for link_to_category in categories_of_products[1:]:  # TODO: 0:4 is just for dev. 0: for finished product
-                        print("LINK:", link_to_category)
+                        # print("LINK:", link_to_category)
                         yield scrapy.Request(link_to_category, callback=self.start_parsing_category, dont_filter=True,
                                              meta={"link_to_category": link_to_category})
-                        exit()
+                        # exit()
 
             # output file of menu items
             with open("menu_items.txt", "w") as f:
@@ -68,14 +71,15 @@ class CottononSpider(scrapy.Spider):
         # 189 / 48 = 3.93. There are 4 pages. Therefore, deploy a ceil() function.
         #
         path_to_total_entries = "//span[@class='paging-information-items']/text()"
-        total_entries_in_category = response.xpath().extract(path_to_total_entries)[0].strip("\n").split(" ")[0]
+        total_entries_in_category = response.xpath(path_to_total_entries).extract()[0].strip("\n").split(" ")[0]
         print("T:", total_entries_in_category)
-        pages = ceil(total_entries_in_category / 48)
+        pages = ceil(int(total_entries_in_category) / 48)
         # assemble list of links for spider to visit
         base_url_for_category = response.meta["link_to_category"]
         links_to_pages_in_this_category = []
         for page_number in range(1, pages + 1):
-            paginated_links = base_url_for_category + "?start=" + page_number * 48 + "&sz=48"
+            start_value = page_number * 48
+            paginated_links = base_url_for_category + "?start=" + str(start_value) + "&sz=48"
             links_to_pages_in_this_category.append(paginated_links)
 
         # ### Handle the first page in the category, which we are already on.
@@ -84,18 +88,19 @@ class CottononSpider(scrapy.Spider):
         # gets between 1 and 48 SelectorLists, depending on how many products are on the page.
         product_tiles_from_the_page = response.xpath(tile_path)
         # FIXME: this naming seems wrong. it's feeding pages into the for loop, so its name should be "pages" ... and yet i was expecting tiles?
-        all_products_from_category = []
         for page in product_tiles_from_the_page[0:1]:  # TODO: remove 0:3 when done developing. its just there to make things run faster
-            new_products = self.convert_product_tiles_from_this_page_to_items(page)  # FIXME: this is currently printing an item that contains the name of every product on the page.
-            all_products_from_category.append(new_products)
+            self.convert_product_tiles_from_this_page_to_items(page)  # FIXME: this is currently printing an item that contains the name of every product on the page.
 
         # ### send Scrapy to handle the rest of the pages in the category, sans the first page, which is done
         # TODO: must get Scrapy to send the products from that page back to this function...
+        page_num = 2
         for remaining_link in links_to_pages_in_this_category[1:]:
-            yield scrapy.Request(remaining_link, self.parse_products_on_this_page, dont_filter=True,
-                                 meta={"past_first_page": True})
+            print(remaining_link)
+            yield scrapy.Request(remaining_link, self.parse_further_pages, dont_filter=True,
+                                 meta={"page_number": page_num})
+            page_num = page_num + 1
 
-        return all_products_from_category
+        return None
 
     def convert_product_tiles_from_this_page_to_items(self, product_tiles_from_the_page):
         """takes a Selector containing a product and converts it into an Item"""
@@ -103,6 +108,7 @@ class CottononSpider(scrapy.Spider):
         product_name_path = "//div[@class='product-name']/a[@class='name-link']/text()"
         product_price_path = "//div[@class='product-pricing ']/span[@class='product-sales-price']/text()"
         product_colors_path = "//div[@class='product-colors']/div[@class='product-colours-available']/span/text()"
+        product_link_path = "//div[@class='product-name']/a[@class='name-link']/@href"
 
         # 1 to 48 SelectorLists and Selectors returned
         selector_list_of_products = product_tiles_from_the_page.xpath("//li[@class='grid-tile columns']")
@@ -117,7 +123,41 @@ class CottononSpider(scrapy.Spider):
             name = product.xpath(product_tile_path + product_name_path).extract()
             price = product.xpath(product_tile_path + product_price_path).extract()
             colors = product.xpath(product_tile_path + product_colors_path).extract()
-            for n, p, c in zip(name, price, colors):
-                products_from_page.append(Product(name=n, price=p, colors=c))
+            individual_page_links = product.xpath(product_tile_path + product_link_path).extract()
 
-        return products_from_page
+            for n, p, c, link in zip(name, price, colors, individual_page_links):
+                imgs, ratings = self.retrieve_ratings_and_images_from_product_page(link)
+                products_from_page.append(Product(name=n, price=p, colors=c, img_links=imgs, ratings=ratings))
+
+        # TODO: have this func end by creating a .csv with products from this page
+        # i.e. product_category_-_product_page.csv
+        return None
+
+    def parse_further_pages(self, response):
+        print("Page num: ", response.meta["page_number"])
+        tile_path = "//div[@class='product-tile']"
+        # gets between 1 and 48 SelectorLists, depending on how many products are on the page.
+        product_tiles_from_the_page = response.xpath(tile_path)
+        for page in product_tiles_from_the_page[0:1]:
+            self.convert_product_tiles_from_this_page_to_items(page)
+        # TODO: have this func end by creating a .csv with products from this page
+        return None
+
+    def retrieve_ratings_and_images_from_product_page(self, link_to_page):
+        page = requests.get(link_to_page)
+        soup = BeautifulSoup(page, "html.parser")
+
+        product_image_tags = soup.find_all("img", {"class": "primary-image"})
+        srcs = []
+        for img in product_image_tags:
+            srcs.append(img["src"])
+
+        ratings_divs = soup.find_all("div", {"class": "bv-inline-histogram-ratings-score"})
+        ratings = []
+        stars = 5
+        for div in ratings_divs:
+            rating = {stars: div.find("span").get_text()}
+            ratings.append(rating)
+            stars = stars - 1
+
+        return srcs, ratings
